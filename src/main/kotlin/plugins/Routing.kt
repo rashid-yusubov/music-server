0b360fe77d3fcd5c110a1f6cd6c621d9d884d7ec
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-27 17:32:39 +0300

    feat(api): add swagger documentation
    Configure Swagger UI documentation for REST API endpoints.

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -3,10 +3,16 @@ package com.rashidyusubov.musicserver.plugins
 import com.rashidyusubov.musicserver.presentation.routes.tracksRoutes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import io.ktor.server.plugins.swagger.*
 
 fun Application.configureRouting() {
 
     routing {
+
+        swaggerUI(
+            path = "swagger",
+            swaggerFile = "openapi/documentation.yaml"
+        )
 
         tracksRoutes()
     }
