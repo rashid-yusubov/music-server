2cce3ba98119868f0c7b8e902c4eaba252da84a3
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-27 15:31:11 +0300

    feat(core): setup clean architecture structure
    Refactor Ktor project structure using Clean Architecture principles and modular plugin configuration.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,16 @@
 package com.rashidyusubov.musicserver
 
-import io.ktor.server.engine.*
+import com.rashidyusubov.musicserver.plugins.configureMonitoring
+import com.rashidyusubov.musicserver.plugins.configureRouting
+import com.rashidyusubov.musicserver.plugins.configureSerialization
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
 }
---
+++ src/main/kotlin/plugins/Serialization.kt
@@ -0,0 +1,7 @@
+package com.rashidyusubov.musicserver.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSerialization() {
+
+}
