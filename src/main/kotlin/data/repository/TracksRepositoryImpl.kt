81407853f68860859a473b72c5febc969834842e
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-27 16:52:22 +0300

    feat(repository): implement tracks repository logic
    Add Exposed database queries and update application tests for tracks repository layer.

--- src/main/kotlin/data/repository/TracksRepositoryImpl.kt
+++ src/main/kotlin/data/repository/TracksRepositoryImpl.kt
@@ -1,19 +1,50 @@
 package com.rashidyusubov.musicserver.data.repository
 
+import com.rashidyusubov.musicserver.data.database.tables.TracksTable
+import com.rashidyusubov.musicserver.data.mapper.toTrack
 import com.rashidyusubov.musicserver.domain.model.Track
 import com.rashidyusubov.musicserver.domain.repository.TracksRepository
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class TracksRepositoryImpl : TracksRepository {
 
     override suspend fun getAllTracks(): List<Track> {
-        return emptyList()
+
+        return transaction {
+
+            TracksTable
+                .selectAll()
+                .map { it.toTrack() }
+        }
     }
 
     override suspend fun getTrackById(id: Int): Track? {
-        return null
+
+        return transaction {
+
+            TracksTable
+                .selectAll()
+                .where {
+                    TracksTable.id eq id
+                }
+                .map { it.toTrack() }
+                .singleOrNull()
+        }
     }
 
     override suspend fun searchTracks(query: String): List<Track> {
-        return emptyList()
+
+        return transaction {
+
+            TracksTable
+                .selectAll()
+                .where {
+                    TracksTable.title like "%$query%"
+                }
+                .map { it.toTrack() }
+        }
     }
 }
