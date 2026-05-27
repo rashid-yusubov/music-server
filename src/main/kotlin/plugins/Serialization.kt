d450d2d721aa8f20cbbbf0de9ec2235075484fa5
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-27 17:03:50 +0300

    fix(api): configure json serialization
    Setup Ktor ContentNegotiation plugin for JSON API responses.

--- src/main/kotlin/plugins/Serialization.kt
+++ src/main/kotlin/plugins/Serialization.kt
@@ -1,7 +1,14 @@
 package com.rashidyusubov.musicserver.plugins
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun Application.configureSerialization() {
 
+    install(ContentNegotiation) {
+
+        json()
+    }
 }
