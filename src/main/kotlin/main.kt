cfde68c376cf6f544996e94c07ab18dcd5dd7bbf
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-27 14:56:44 +0300

    chore: initialize ktor backend project

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+package com.rashidyusubov.musicserver
+
+import io.ktor.server.engine.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
